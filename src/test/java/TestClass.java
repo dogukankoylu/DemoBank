fa9e5abcab627a8239d68f9954dbd2c6a3dc2e75
CKOYLU <CKOYLU@5CG82839QG>
2024-09-18 23:52:52 +0300

    update

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -4,7 +4,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Account {
+public class TestClass {
     WebDriver driver;
     PageFactory pageFactory;
     PageObjectModel pageObjectModel;
